d9dcaeec3bf8bd3f1a9b250c9b35cda3e2b1a3df
Ayushkg1 <89448835+Ayushkg1@users.noreply.github.com>
2021-09-04 13:08:25 +0530

    Update settings.py

--- findingleader/settings.py
+++ findingleader/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jk@ap#rm)f#+l-@v$94r54#mtlv(r7l)#gkcdnt2(^hovp5f=5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['findingtheleader.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
